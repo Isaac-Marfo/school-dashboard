d1ebf0c48aa3b6e77f70e0e4ec434b65425961ae
Isaac Marfo <bempongisaac10@gmail.com>
2024-11-20 23:25:49 +0000

    feature: added the layout and some pages

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,54 @@
+import Menu from "@/components/Menu";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Lama Dev School Management Dashboard",
+  description: "Next.js School Management System",
+};
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-screen">
+      {/* LEFT  */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] h-screen  bg-slate-100 overflow-scroll">
+        <Link
+          href="/"
+          className="flex items-center   lg:justify-start gap-2 p-2 hover:bg-slate-200 text-gray-600"
+        >
+          <Image src={"/logo.png"} alt="logo" width={32} height={32} />
+          <span className="font-bold hidden lg:block">School Name</span>
+        </Link>
+        <Menu />
+      </div>
+
+      {/* RIGHT  */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] h-screen  bg-slate-200">
+        {children}
+      </div>
+    </div>
+
+    // <div>
+    //   <div className=" absolute bottom-0 w-full z-50 min-h-[40px] sm:hidden  bg-orange-500">
+    //     <h1>bottom nav</h1>
+    //   </div>
+    //   <div className="grid grid-cols-12 h-screen">
+    //     {/* LEFT  */}
+    //     <div className=" hidden sm:col-span-2 sm:block  bg-slate-400">left</div>
+
+    //     {/* RIGHT  */}
+    //     <div className="col-span-12 sm:col-span-10   bg-slate-200">
+    //       {children}
+    //     </div>
+    //   </div>
+    // </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* <div className="w-full h-screen shadow bg-gray-500 grid grid-cols-12">
+          <div className="col-span-2 hidden sm:block h-full" bg-gray-500>
+            sidebar
+          </div>
+          <div className=" gird grid-cols-2 col-span-12 sm:col-span-10  bg-slate-100">
+            <div className="sticky top-0 z-50 min-h-[40px]  bg-orange-500">
+              header
+            </div>
+            <div className="bg-sky-500 h-full">{children}</div>
+          </div>
+        </div> */}
+        
+        {children}
+      </body>
     </html>
   );
 }
